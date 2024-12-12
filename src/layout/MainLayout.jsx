79de4f438a290f9d0e02ef5917c815c1dcc1f977
Ashraful Islam <ashrafulislam.dev1@gmail.com>
2024-12-12 23:21:21 +0600

    Added a navbar and setup the firebase

--- src/layout/MainLayout.jsx
+++ src/layout/MainLayout.jsx
@@ -1,10 +1,12 @@
 import React from "react";
 import { Outlet } from "react-router-dom";
+import Navbar from "../pages/shared/Navbar";
 
 const MainLayout = () => {
   return (
     <div>
       {/* Navbar */}
+      <Navbar />
       <Outlet />
       {/* Footer */}
     </div>
