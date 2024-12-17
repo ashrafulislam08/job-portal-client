273ed08bc44a18b0da406219459a6b5e264a08b4
Ashraful Islam <ashrafulislam.dev1@gmail.com>
2024-12-17 20:31:06 +0600

    Added view application page for view hr posted jobs applied info

--- src/pages/ViewApplications/ViewApplications.jsx
+++ src/pages/ViewApplications/ViewApplications.jsx
@@ -1,7 +1,72 @@
 import React from "react";
+import { useLoaderData } from "react-router-dom";
+import { toast } from "react-toastify";
 
 const ViewApplications = () => {
-  return <div>ViewApplications</div>;
+  const applications = useLoaderData();
+
+  const handleStatusUpdate = (e, id) => {
+    const data = {
+      status: e.target.value,
+    };
+
+    fetch(`http://localhost:3000/job-applications/${id}`, {
+      method: "PATCH",
+      headers: {
+        "content-type": "application/json",
+      },
+      body: JSON.stringify(data),
+    })
+      .then((res) => res.json())
+      .then((data) => {
+        if (data.modifiedCount) {
+          toast.success("Status has been updated");
+        }
+      });
+  };
+
+  return (
+    <div>
+      <h2 className="text-3xl">
+        Applications for this job: {applications.length}
+      </h2>
+      <div className="overflow-x-auto">
+        <table className="table">
+          {/* table head */}
+          <thead>
+            <tr>
+              <th></th>
+              <th>Email</th>
+              <th>Status</th>
+              <th>Update Status</th>
+            </tr>
+          </thead>
+          <tbody>
+            {applications.map((app, index) => (
+              <tr key={app._id}>
+                <th>{index + 1}</th>
+                <th>{app.applicant_email}</th>
+                <td>Quality Control Specialist</td>
+                <td>
+                  <select
+                    onChange={(e) => handleStatusUpdate(e, app._id)}
+                    defaultValue={app.status || "Change status"}
+                    className="select select-bordered select-xs w-full max-w-xs"
+                  >
+                    <option disabled>Change Status</option>
+                    <option>Under Review</option>
+                    <option>Set Interview</option>
+                    <option>Hired</option>
+                    <option>Rejected</option>
+                  </select>
+                </td>
+              </tr>
+            ))}
+          </tbody>
+        </table>
+      </div>
+    </div>
+  );
 };
 
 export default ViewApplications;
